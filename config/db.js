fe4e0f8d2e6a46eb8ea2ea96cb86c7306475ff36
unknown <sushant_chaturvedi@softprodigy.com>
2025-03-27 13:21:31 +0530

    Updated As Asked

--- config/db.js
+++ config/db.js
@@ -1,16 +1,12 @@
 const mongoose = require("mongoose");
- 
- const dotenv = require("dotenv");
- dotenv.config();
- 
- const connectDB = async () => {
-     try {
-         await mongoose.connect(process.env.MONGO_URI)
-         console.log("MongoDB Connected");
-     } catch (error) {
-         console.log({ message : "MongoDB Connection Error:" });
-     }
- };
- 
- module.exports = connectDB;
- 
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI)
+        console.log("MongoDB Connected");
+    } catch (error) {
+        console.log({ message: "MongoDB Connection Error:" });
+    }
+};
+
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,21 +1,21 @@
 const express = require("express");
- const dotenv = require("dotenv");
- const cors = require("cors");
- const bodyParser = require("body-parser");
- const connectDB = require("./config/db");
- const authRoutes = require("./routes/auth.routes");
- const userRoutes = require("./routes/user.routes");
- 
- dotenv.config();
- connectDB();
- 
- const app = express();
- 
- app.use(cors());
- app.use(bodyParser.json());
- 
- app.use("/auth", authRoutes);
- app.use("/users", userRoutes);
- 
- const PORT = process.env.PORT || 5000;
- app.listen(PORT, () => console.log(`Server uding on port ${PORT}`));
+const dotenv = require("dotenv");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const connectDB = require("./config/db");
+const authRoutes = require("./routes/auth.routes");
+const userRoutes = require("./routes/user.routes");
+
+dotenv.config();
+connectDB();
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server uding on port ${PORT}`));
