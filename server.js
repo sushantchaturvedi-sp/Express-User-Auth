a9d55c9b86b26aad6db5a5307ecd7a43f023d7bf
unknown <sushant_chaturvedi@softprodigy.com>
2025-03-27 13:24:00 +0530

    Updated As Asked

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
+const bodyParser = require("body-parser");
 const connectDB = require("./config/db");
 const authRoutes = require("./routes/auth.routes");
 const userRoutes = require("./routes/user.routes");
@@ -11,7 +12,7 @@ connectDB();
 const app = express();
 
 app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json());
 
 app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
